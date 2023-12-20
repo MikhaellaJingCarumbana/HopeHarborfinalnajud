cd10190e5382258ecffa8756c9389e16f26703ec
Mikhaella Jing Carumbana <120296220+MikhaellaJingCarumbana@users.noreply.github.com>
2023-12-20 18:56:34 +0800

    Delete HopeHarbor1 directory

--- HopeHarbor1/HopeHarbor1/AdminStaff/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'AdminStaff'
-
-urlpatterns = [
-    path('reg/', views.AdminStaffRegistration.as_view(), name='reg'),
-    path('login/', views.AdminStaffLogin.as_view(), name='login')
-]
